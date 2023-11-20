ab6a73d1190087a9de7f0e6d550246a7116ead0e
DursuncanAydemir <dursuncan.aydemir28@gmail.com>
2023-11-20 13:12:47 +0300

    Add project files.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+public class AutofacBusinessModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+        builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle = Yaptığın yazılıma yeni bir özellik ekliyorsan mevcuttaki hiçbir koduna dokunamazsın!!!
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+
+            //CategoryTest();
+
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetProductDetails();
+
+            if(result.Success==true) 
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+            }
+            else 
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
+        }
+    }
+}
