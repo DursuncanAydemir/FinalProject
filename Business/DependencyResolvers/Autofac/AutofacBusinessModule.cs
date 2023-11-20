4e6740252a50d34115fe4afa12b4f2131f4aebca
DursuncanAydemir <dursuncan.aydemir28@gmail.com>
2023-11-20 15:51:39 +0300

    test

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -4,6 +4,9 @@ using Business.Concrete;
 using Microsoft.Extensions.DependencyInjection;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Autofac.Extras.DynamicProxy;
 
 public class AutofacBusinessModule : Module
 {
@@ -11,5 +14,11 @@ public class AutofacBusinessModule : Module
     {
         builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
         builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+        builder.RegisterAssemblyTypes(ThisAssembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
     }
 }
